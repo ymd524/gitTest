e12218ff2e0d40dad8daec6a6560f797b0e4d71d
Richard Ryan Garcia <atticus1129@gmail.com>
2015-02-03 12:24:39 -0600

    First commit

---
+++ compile.sh
@@ -0,0 +1,1 @@
+javac src/core/*.java -d ./bin
---
+++ src/core/MyDemo.java
@@ -0,0 +1,12 @@
+package core;
+
+public class MyDemo {
+
+	public MyDemo() {
+		System.out.println("Hello world!");
+	}
+	
+	public static void main(String [] args) {
+		MyDemo app = new MyDemo();
+	}
+}
